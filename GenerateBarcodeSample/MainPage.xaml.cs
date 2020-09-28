7a1abb3c1b88fa1694072db0cc08200fbc760a10
Erno Soinila <erno.soinila@softability.fi>
2020-09-28 16:12:14 +0300

    Added a button
    Now shows just a pop-up.

--- GenerateBarcodeSample/MainPage.xaml.cs
+++ GenerateBarcodeSample/MainPage.xaml.cs
@@ -10,9 +10,17 @@ namespace GenerateBarcodeSample
 {
     public partial class MainPage : ContentPage
     {
+        private string _myText = "The secret is to use your arse muscless in lifting stuff";
+        public string MyText { get { return _myText; } set { _myText = value; } }
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        async void OnButtonClicked(object sender, EventArgs args)
+        {
+            await DisplayAlert("Alert", "You pressed a button", "OK");
         }
     }
 }
